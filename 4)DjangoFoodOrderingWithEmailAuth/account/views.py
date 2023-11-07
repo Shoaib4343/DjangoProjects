4c88a92ebe6185519ee96cd608d40a566949fe53
Shoaib4343 <133510596+Shoaib4343@users.noreply.github.com>
2023-11-07 18:45:24 +0500

    Delete 4)DjangoFoodOrderingWithEmailAuth directory

--- 4)DjangoFoodOrderingWithEmailAuth/account/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render, redirect
-# from django.contrib.auth import get_user_model
-# User = get_user_model
-def index_page(request):
-    return render(request, 'index.html')
